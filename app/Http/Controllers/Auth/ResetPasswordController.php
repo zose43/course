8e7d307726fee267549443419b742627780d2a1b
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2022-11-07 22:12:20 +0300

    add actions

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -6,7 +6,7 @@ use Illuminate\View\View;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Password;
-use Illuminate\Auth\Events\PasswordReset;
+use Domain\Auth\Actions\ResetPasswordAction;
 use App\Http\Requests\ResetPasswordFormRequest;
 
 class ResetPasswordController extends Controller
@@ -16,20 +16,9 @@ class ResetPasswordController extends Controller
         return view('auth.reset-password', compact('token', 'email'));
     }
 
-    public function handle(ResetPasswordFormRequest $request): RedirectResponse
+    public function handle(ResetPasswordFormRequest $request, ResetPasswordAction $action): RedirectResponse
     {
-        $status = Password::reset(
-            $request->only('email', 'password', 'password_confirmation', 'token'),
-            static function ($user, $password) {
-                $user->forceFill([
-                    'password' => bcrypt($password),
-                ])->setRememberToken(str()->random(60));
-
-                $user->save();
-                event(new PasswordReset($user));
-            }
-        );
-
+        $status = $action($request->only('email', 'password', 'password_confirmation', 'token'));
         if ($status === Password::PASSWORD_RESET) {
             flash()->info(__($status));
 

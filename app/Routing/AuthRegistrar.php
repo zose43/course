8949fd2f60a167aab76f43fbead85d463a468023
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2022-12-03 18:18:41 +0300

    fx auth routing

--- app/Routing/AuthRegistrar.php
+++ app/Routing/AuthRegistrar.php
@@ -16,33 +16,49 @@ class AuthRegistrar implements RouteRegistrar
 
     public function map(Registrar $registrar): void
     {
-        Route::middleware('web')->group(function () {
+        Route::middleware([
+            'web',
+            'throttle:auth',
+            'guest',
+        ])->group(function () {
             Route::controller(SignInController::class)->group(static function () {
-                Route::get('/login', 'page')->name('login');
-                Route::post('/login', 'handle')->name('login.handle')->middleware('throttle:auth');
-
-                Route::delete('/logout', 'logOut')->name('log-out');
+                Route::get('/login', 'page')
+                    ->name('login');
+                Route::post('/login', 'handle')
+                    ->name('login.handle');
             });
 
             Route::controller(SignUpController::class)->group(function () {
-                Route::get('/sign-up', 'page')->name('sign-up');
-                Route::post('/sign-up', 'handle')->name('sign-up.handler')->middleware('throttle:auth');
+                Route::get('/sign-up', 'page')
+                    ->name('sign-up');
+                Route::post('/sign-up', 'handle')
+                    ->name('sign-up.handler');
             });
 
             Route::controller(ForgotPasswordController::class)->group(function () {
-                Route::get('/forgot-password', 'page')->middleware('guest')->name('forgot');
-                Route::post('/forgot-password', 'handle')->middleware('guest')->name('forgot.handler');
+                Route::get('/forgot-password', 'page')
+                    ->name('forgot');
+                Route::post('/forgot-password', 'handle')
+                    ->name('forgot.handler');
             });
 
             Route::controller(ResetPasswordController::class)->group(function () {
-                Route::get('/reset/{token}/{email}', 'page')->middleware('guest')->name('reset');
-                Route::post('/reset', 'handle')->middleware('guest')->name('reset.handler');
+                Route::get('/reset/{token}/{email}', 'page')
+                    ->name('reset');
+                Route::post('/reset', 'handle')
+                    ->name('reset.handler');
             });
 
             Route::controller(SocialAuthController::class)->group(function () {
-                Route::get('/auth/socialite/{driver}', 'redirect')->middleware('guest')->name('socialite.github');
-                Route::get('/auth/socialite/{driver}/callback', 'callback')->middleware('guest')->name('socialite.callback');
+                Route::get('/auth/socialite/{driver}', 'redirect')
+                    ->name('socialite.github');
+                Route::get('/auth/socialite/{driver}/callback', 'callback')
+                    ->name('socialite.callback');
             });
         });
+
+        Route::delete('/logout', [SignInController::class, 'logOut'])
+            ->middleware('web')
+            ->name('log-out');
     }
 }

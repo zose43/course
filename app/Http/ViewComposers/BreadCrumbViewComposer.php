129427ae3156d1c0ea2389b1c831576f382f8b2e
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2022-12-04 20:18:45 +0300

    fx breadcrumbs composer, fx category view

--- app/Http/ViewComposers/BreadCrumbViewComposer.php
+++ app/Http/ViewComposers/BreadCrumbViewComposer.php
@@ -6,35 +6,17 @@ use Illuminate\View\View;
 use App\Breadcrumbs\BreadCrumb;
 use Illuminate\Support\Collection;
 use App\Breadcrumbs\BreadCrumbItem;
-use Domain\Catalog\Models\Category;
 
 class BreadCrumbViewComposer
 {
-    private function getCategorySlug(): string
-    {
-        return str(request()?->path())
-            ->match('/catalog\/?(.*?)($|\/|\?)/')
-            ->value();
-    }
-
-    private function getCategory(Collection $collection): ?Category
-    {
-        $slug = $this->getCategorySlug();
-
-        return $collection->filter(fn(Category $v) => $v->slug === $slug)
-            ->first();
-    }
-
     public function compose(View $view): View|Collection
     {
-        // TODO fix category link
-        $categories = $view->getData()['categories'];
-        $category = $this->getCategory($categories);
+        $category = request()?->route()?->parameter('category');
         $breadcrumbs = BreadCrumb::make()
             ->add(BreadCrumbItem::make('Главная', route('home')))
             ->add(BreadCrumbItem::make('Каталог', route('catalog')))
             ->addIf($category !== null,
-                BreadCrumbItem::make($category?->title ?? '', $category?->slug ?? ''));
+                BreadCrumbItem::make($category?->slug ?? '', route('catalog', $category)));
 
         return $view->with('breadcrumbs', $breadcrumbs);
     }
--- resources/views/catalog/shared/category.blade.php
+++ resources/views/catalog/shared/category.blade.php
@@ -1,2 +1,4 @@
 <a class="p-3 sm:p-4 2xl:p-6 rounded-xl bg-card hover:bg-pink text-xxs sm:text-xs lg:text-sm text-white font-semibold"
-   href="#">{{ $item->title }}</a>
+   href="{{ route('catalog', $item) }}">
+    {{ $item->title }}
+</a>

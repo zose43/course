1e54a39967d89a73cb3467acd7b16e27fc6541f0
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2022-12-17 17:52:36 +0300

    add viewModel from spatie, update catalog controller

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -2,28 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Domain\Product\Models\Product;
 use Domain\Catalog\Models\Category;
-use Domain\Catalog\ViewModels\CategoryViewModel;
+use App\ViewModels\CatalogViewModel;
 
 class CatalogController extends Controller
 {
     public function __invoke(?Category $category)
     {
-        $products = Product::query()
-            ->withCategory($category)
-            ->search()
-            ->filtered()
-            ->sorted()
-            ->paginate(6);
-
-        $categories = CategoryViewModel::make()
-            ->catalog();
-
-        return view('catalog.index', compact(
-            'products',
-            'categories',
-            'category'
-        ));
+        return (new CatalogViewModel($category))->view('catalog.index');
     }
 }

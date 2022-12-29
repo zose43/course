ea3dedf0e47931ea2e8ee7400a263070320ced54
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2022-12-29 17:55:24 +0300

    update caer_controller tests

--- tests/Feature/App/Http/Controllers/CartControllerTest.php
+++ tests/Feature/App/Http/Controllers/CartControllerTest.php
@@ -40,13 +40,99 @@ class CartControllerTest extends TestCase
      */
     public function is_not_empty_cart(): void
     {
+        $quantity = 2;
         $product = ProductFactory::new()
             ->create();
-        cart()->add($product, 1);
+        cart()->add($product, $quantity);
 
         $this->get(action([CartController::class, 'index']))
-            ->assertOk()
-            ->assertViewIs('cart.index')
-            ->assertViewHas('items', cart()->cartItems());
+            ->assertViewHas('items', cart()->cartItems())
+            ->assertSee($product->title)
+            ->assertSee($product->price)
+            ->assertSee(cart()->amount())
+            ->assertSee("$quantity шт");
+    }
+
+    /**
+     * @test
+     * @covers \App\Http\Controllers\CartController::add()
+     */
+    public function is_added_successfully(): void
+    {
+        $quantity = 2;
+        $product = ProductFactory::new()
+            ->create();
+
+        $this->assertEquals(0, cart()->count());
+
+        $this->post(
+            action([CartController::class, 'add'], $product),
+            [
+                'quantity' => $quantity,
+            ])
+            ->assertSessionHas('course_flash_msg', 'Товар добавлен в корзину');
+    }
+
+    /**
+     * @test
+     * @covers \App\Http\Controllers\CartController::quantity()
+     */
+    public function is_quantity_changed(): void
+    {
+        $quantity = 2;
+        $product = ProductFactory::new()
+            ->create();
+        cart()->add($product, $quantity);
+
+        $this->assertEquals($quantity, cart()->count());
+
+        $this->post(
+            action([CartController::class, 'quantity'], cart()->cartItems()->first()),
+            [
+                'quantity' => $quantity * 2,
+            ])
+            ->assertSessionHas('course_flash_msg', 'Кол-во товара изменено');
+
+        $this->assertEquals($quantity * 2, cart()->count());
+    }
+
+    /**
+     * @test
+     * @covers \App\Http\Controllers\CartController::quantity()
+     */
+    public function is_delete_success(): void
+    {
+        $quantity = 2;
+        $product = ProductFactory::new()
+            ->create();
+        cart()->add($product, $quantity);
+
+        $this->assertEquals($quantity, cart()->count());
+
+        $this->delete(
+            action([CartController::class, 'delete'], cart()->cartItems()->first()))
+            ->assertSessionHas('course_flash_msg', 'Удалено из корзины');
+
+        $this->assertEquals(0, cart()->count());
+    }
+
+    /**
+     * @test
+     * @covers \App\Http\Controllers\CartController::quantity()
+     */
+    public function is_truncate_success(): void
+    {
+        $product = ProductFactory::new()
+            ->create();
+        cart()->add($product,1);
+
+        $this->assertDatabaseCount('cart_items',1);
+
+        $this->delete(
+            action([CartController::class, 'truncate'], cart()->cartItems()->first()))
+            ->assertSessionHas('course_flash_msg', 'Корзина очищена');
+
+        $this->assertDatabaseEmpty('carts');
+        $this->assertDatabaseEmpty('cart_items');
     }
 }

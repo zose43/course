0ea6c294e8956a6875fa7c2dc87ac87fd8828a4c
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-01-18 01:07:06 +0300

    add domain order base classes and temporary realizes

--- src/Domain/Order/Models/DeliveryType.php
+++ src/Domain/Order/Models/DeliveryType.php
@@ -4,16 +4,26 @@ namespace Domain\Order\Models;
 
 use Support\Casts\PriceCast;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Domain\Order\QueryBuilders\DeliveryTypeQueryBuilder;
 
+/**
+ * @method DeliveryTypeQueryBuilder query()
+ */
 class DeliveryType extends Model
 {
-    protected $fillable=[
+    protected $fillable = [
         'price',
         'title',
-        'with_address'
+        'with_address',
     ];
 
-    protected $casts=[
-        'price' => PriceCast::class
+    protected $casts = [
+        'price' => PriceCast::class,
     ];
+
+    public function newEloquentBuilder($query): Builder
+    {
+        return new DeliveryTypeQueryBuilder($query);
+    }
 }
--- src/Domain/Order/Models/PaymentMethod.php
+++ src/Domain/Order/Models/PaymentMethod.php
@@ -3,11 +3,21 @@
 namespace Domain\Order\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Domain\Order\QueryBuilders\PaymentMethodQueryBuilder;
 
+/**
+ * @method PaymentMethodQueryBuilder query()
+ */
 class PaymentMethod extends Model
 {
     protected $fillable = [
         'title',
-        'redirect_to_pay'
+        'redirect_to_pay',
     ];
+
+    public function newEloquentBuilder($query): Builder
+    {
+        return new PaymentMethodQueryBuilder($query);
+    }
 }

d4deda3ab1650714b79362f87f0ad082d1e300bd
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-02-01 23:32:13 +0300

    add migrations

---
+++ database/migrations/2023_02_01_230940_craete_payments.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('payments', static function (Blueprint $table) {
+            $table->id();
+            $table->uuid('payment_id');
+            $table->string('gateway');
+            $table->json('meta')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('payments', static function (Blueprint $table) {
+            if (!app()->isProduction()) {
+                Schema::dropIfExists('payments');
+            }
+        });
+    }
+};
---
+++ database/migrations/2023_02_01_232614_create_payment_history_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('payment_history', static function (Blueprint $table) {
+            $table->id();
+            $table->string('payment_gateway');
+            $table->string('method')
+                ->nullable();
+            $table->json('payload')
+                ->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        if (!app()->isProduction()) {
+            Schema::dropIfExists('payment_history');
+        }
+    }
+};

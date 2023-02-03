d5ddd30c97863b6d36e02a810afae2e05e3a4035
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-02-03 09:24:35 +0300

    fx with template

--- src/Domain/Order/Providers/PaymentServiceProvider.php
+++ src/Domain/Order/Providers/PaymentServiceProvider.php
@@ -4,7 +4,6 @@ namespace Domain\Order\Providers;
 
 use Domain\Order\DTOs\PaymentDataDTO;
 use Illuminate\Support\ServiceProvider;
-use Domain\Order\Payment\PaymentSystem;
 use Domain\Order\Payment\PaymentEventsManager;
 
 class PaymentServiceProvider extends ServiceProvider
@@ -13,7 +12,7 @@ class PaymentServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        PaymentSystem::provider();
+//        PaymentSystem::provider();
 
         PaymentEventsManager::setOnCreating(static function (PaymentDataDTO $data) {
             return $data;
